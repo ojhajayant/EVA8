a0f314a1194e23a0acf3c32b032cc80362d6ab6a
JayO <45680293+ojhajayant@users.noreply.github.com>
2023-01-25 09:27:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eva8',
+    version='0.0.0',
+    description='My private package from private EVA repo',
+    url='git@github.com:ojhajayant/EVA8.git',  
+    author='Jayant Ojha',
+    author_email='jayantojha78@gmail.com',
+    license='unlicense',
+    packages=find_packages(),
+    zip_safe=False
+)
